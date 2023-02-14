495a7f3faae77b51eb5cc03c37c50db6a28d3f6e
naushadraien <naushadraien5@gmail.com>
2023-02-14 20:39:11 +0545

    second commit and made changes in files of backend

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017"
-const connectToMongo =()=>{
+
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true"
+
+const connectToMongo = ()=>{
+    mongoose.set('strictQuery', false); // Set strictQuery to false
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to Mongo Successfully");
-    } )
+    })
 }
 
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const connectToMongo = require('./db');
-connectToMongo();
 const express = require('express')
 
+connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Rehan!')
-})
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
